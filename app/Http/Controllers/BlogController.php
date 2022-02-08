bb4cb9efe2d8b5a19d341d931914eb09b43e750a
martin.tellez <martin.tellez.g86@icloud.com>
2022-02-08 08:28:08 -0600

    Paginador termiando

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -12,7 +12,7 @@ class BlogController extends Controller
     public function index()
     {
         
-        $posts = Post::paginate(5);
+        $posts = Post::paginate(3);
         return view('blog.blog')->with('posts',$posts);
     }
 
--- resources/views/paginate.blade.php
+++ resources/views/paginate.blade.php
@@ -1,89 +1,44 @@
 <div class="col-lg-12 col-md-12">
-                    <div class="pagination-area">
-                        {{-- Previous Page Link --}}
-                        @if ($paginator->onFirstPage())
-                        <a href="#" class="prev page-numbers">
-                            <i class='bx bx-chevron-left'></i>
-                        </a>
-                        @else
-                            <a class="prev page-numbers" href="{{ $paginator->previousPageUrl() }}" rel="prev" aria-label="@lang('pagination.previous')">
-                                <i class='bx bx-chevron-left'></i> 
-                            </a>
+    <div class="pagination-area">
+        {{-- Previous Page Link --}}
+        @if ($paginator->onFirstPage())
+        <a href="#" class="prev page-numbers">
+            <i class='bx bx-chevron-left'></i>
+        </a>
+        @else
+            <a class="prev page-numbers" href="{{ $paginator->previousPageUrl() }}" rel="prev" aria-label="@lang('pagination.previous')">
+                <i class='bx bx-chevron-left'></i> 
+            </a>
 
-                        @endif
-                        @foreach ($elements as $element)
-                            {{-- "Three Dots" Separator --}}
-                            @if (is_string($element))
-                                <li class="disabled" aria-disabled="true"><span>{{ $element }}</span></li>
-                            @endif
-
-                            {{-- Array Of Links --}}
-                            @if (is_array($element))
-                                @foreach ($element as $page => $url)
-                                    @if ($page == $paginator->currentPage())
-                                        <li class="active" aria-current="page"><span>{{ $page }}</span></li>
-                                    @else
-                                        <li><a href="{{ $url }}">{{ $page }}</a></li>
-                                    @endif
-                                @endforeach
-                            @endif
-                        @endforeach
-
-
-
-                        <a href="#" class="page-numbers">1</a>
-                        <span class="page-numbers current" aria-current="page">2</span>
-                        <a href="#" class="page-numbers">3</a>
-                        <a href="#" class="page-numbers">4</a>
-                        <a href="#" class="next page-numbers">
-                            <i class='bx bx-chevron-right'></i>
-                        </a>
-                    </div>
-                </div>
-
-                @if ($paginator->hasPages())
-    <nav>
-        <ul class="pagination">
-            {{-- Previous Page Link --}}
-            @if ($paginator->onFirstPage())
-                <li class="disabled" aria-disabled="true" aria-label="@lang('pagination.previous')">
-                    <span aria-hidden="true">&lsaquo;</span>
-                </li>
-            @else
-                <li>
-                    <a href="{{ $paginator->previousPageUrl() }}" rel="prev" aria-label="@lang('pagination.previous')">&lsaquo;</a>
-                </li>
+        @endif
+        @foreach ($elements as $element)
+            {{-- "Three Dots" Separator --}}
+            @if (is_string($element))
+                <li class="disabled" aria-disabled="true"><span>{{ $element }}</span></li>
             @endif
 
-            {{-- Pagination Elements --}}
-            @foreach ($elements as $element)
-                {{-- "Three Dots" Separator --}}
-                @if (is_string($element))
-                    <li class="disabled" aria-disabled="true"><span>{{ $element }}</span></li>
-                @endif
-
-                {{-- Array Of Links --}}
-                @if (is_array($element))
-                    @foreach ($element as $page => $url)
-                        @if ($page == $paginator->currentPage())
-                            <li class="active" aria-current="page"><span>{{ $page }}</span></li>
-                        @else
-                            <li><a href="{{ $url }}">{{ $page }}</a></li>
-                        @endif
-                    @endforeach
-                @endif
-            @endforeach
-
-            {{-- Next Page Link --}}
-            @if ($paginator->hasMorePages())
-                <li>
-                    <a href="{{ $paginator->nextPageUrl() }}" rel="next" aria-label="@lang('pagination.next')">&rsaquo;</a>
-                </li>
-            @else
-                <li class="disabled" aria-disabled="true" aria-label="@lang('pagination.next')">
-                    <span aria-hidden="true">&rsaquo;</span>
-                </li>
+            {{-- Array Of Links --}}
+            @if (is_array($element))
+                @foreach ($element as $page => $url)
+                    @if ($page == $paginator->currentPage())
+                        <span class="page-numbers current" aria-current="page">{{ $page }}</span>
+                    @else
+                        <a href="{{ $url }}" class="page-numbers">{{ $page }}</a>
+                    @endif
+                @endforeach
             @endif
-        </ul>
-    </nav>
-@endif
+        @endforeach
+        {{-- Next Page Link --}}
+        @if ($paginator->hasMorePages())
+    
+            <a href="{{ $paginator->nextPageUrl() }}"rel="next" aria-label="@lang('pagination.next')" class="next page-numbers">
+                <i class='bx bx-chevron-right'></i>
+            </a>
+        @else
+
+            <a href="#" class="next page-numbers">
+                <i class='bx bx-chevron-right'></i>
+            </a>
+        @endif
+    </div>
+</div>
