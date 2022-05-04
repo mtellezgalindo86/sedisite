703c42e5252534bbff164b8bb12308b957a53b19
martin.tellez <martin.tellez.g86@icloud.com>
2022-05-04 12:46:03 -0500

    se agrega log

--- resources/views/components/main/header.blade.php
+++ resources/views/components/main/header.blade.php
@@ -1,6 +1,6 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
+<link rel="icon" type="image/png" href="{{'../assets/img/sedi.png'}}" />
 <!-- Bootstrap CSS -->
 <link rel="stylesheet" href="{{'../assets/css/bootstrap.min.css'}}">
 <!-- Animate CSS -->
